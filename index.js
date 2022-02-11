e345312cd37395102222c3833a2525265105c01d
DanBaDo <bytes4life@.pm.me>
2022-02-11 13:33:48 +0100

    Done

--- index.js
+++ index.js
@@ -1,37 +1,21 @@
-let hovers = [];
-
-function countHovers (ev) {
-    let idx = parseInt(ev.target.id);
-    if (typeof(hovers[idx]) === "number") {
-        hovers[idx]++;
-    } else {
-        hovers[idx] = 1;
-    }
-    console.log(`Entering ${idx} ${hovers[idx]} times`);
-}
-
-function showCounter () {
-    console.log(hovers);
-}
-
 function main () {
     console.log("Starting...");
 
-    const body = document.querySelector("body");
-    const elements = Array.from(body.querySelectorAll("*"));
+    const elements = Array.from(document.body.querySelectorAll("*"));
 
-    for (let idx in elements) {
-        elements[idx].id = idx;
-        elements[idx].addEventListener("pointerenter",countHovers);
-    }
-
-    const button = document.createElement("button");
-    button.innerText = "Show counter"
-    button.style.position = "fixed";
-    button.style.bottom = "20px";
-    button.style.right = "20px";
-    button.addEventListener("click",showCounter);
-    body.appendChild(button);
+    const counters = elements.map(
+        (item) => {
+            const obj = {
+                element: item,
+                counter: 0,
+            }
+            item.addEventListener("pointerenter",()=>{
+                obj.counter++;
+                console.log(obj)
+            });
+            return obj
+        }
+    )
 }
 
 window.addEventListener("load", main);
